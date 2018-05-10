2d2222bc6fed4b8d706d80d33d7ff973d9cda515
Ihar Hrunt <Ihar_Hrunt@epam.com>
2018-05-10 15:33:02 +0300

    Initial commit

---
+++ FriendOrganizer.UI/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using FriendOrganizer.UI.Startup;
+using System.Windows;
+
+namespace FriendOrganizer.UI
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var bootstrapper = new Bootstrapper();
+            var container = bootstrapper.Bootstrap();
+            var mainWindow = container.Resolve<MainWindow>();
+            //var mainWindow = new MainWindow(new MainViewModel(new FriendDataService()));
+            mainWindow.Show();
+        }
+    }
+}
---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,18 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.Generic;
+
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            //TODO: Load data from real db
+            yield return new Friend { FirstName = "Ihar1", LastName = "Hrunt1" };
+            yield return new Friend { FirstName = "Ihar2", LastName = "Hrunt2" };
+            yield return new Friend { FirstName = "Ihar3", LastName = "Hrunt3" };
+            yield return new Friend { FirstName = "Ihar4", LastName = "Hrunt4" };
+        }
+    }
+}
